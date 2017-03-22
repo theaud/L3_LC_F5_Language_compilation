5d003bccc64220ccfd9498616b3d6f1c4ef6ab39
theaud <brionmathieu@gmail.com>
2017-03-22 14:12:33 +0100

    lecture de fichier

--- Grammaire.h
+++ Grammaire.h
@@ -3,12 +3,13 @@
 #ifndef L3_LC_F5_LANGUAGE_COMPILATION_GRAMMAIRE_H
 #define L3_LC_F5_LANGUAGE_COMPILATION_GRAMMAIRE_H
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Grammaire {
 
-
+vector <string> Gramaire;
 
 
 
