bc5bfba3ff9d5a531e647fca9e727c52bbbe8b52
theaud <brionmathieu@gmail.com>
2017-03-22 17:04:35 +0100

    lecture de fichier

---
+++ Camille.cpp
@@ -0,0 +1,30 @@
+//
+// Created by mathieu on 22/03/2017.
+//
+
+#include "Camille.h"
+
+
+Grammaire Camille::Derecusivite_gauche(Grammaire entrer)
+{
+    Grammaire Derecusive;
+
+
+    for(Regle tester():Derecusive.List_Regle)
+    {
+
+
+
+    }
+    string current;
+    string alpha,beta;
+    bool trouver=false;
+    bool option =false;
+
+
+
+
+
+
+
+}
